b18c01dd711e4e987c53381e80343d2570d8a84f
Nirzi <nirzzii@mail.ru>
2025-01-29 17:26:31 +0500

    Финальная версия

--- Инкапсуляция.py
+++ Инкапсуляция.py
@@ -45,4 +45,27 @@ class ImageCompressor:
         if not (0 <= value <= 100):
             raise ValueError("Качество должно быть в диапазоне 0-100.")
         self.__quality = value
-        
+
+def main(input_path: str, quality: int = 50) -> None:
+    """
+    Основная функция программы.
+    """
+    compressor = ImageCompressor(quality)
+    input_path = input_path.strip('"')  # Удаляем кавычки, если есть
+
+    if os.path.exists(input_path):
+        if os.path.isfile(input_path):
+            print(f"Обрабатываем файл: {input_path}")
+            output_path = os.path.splitext(input_path)[0] + '.heic'
+            compressor.compress_image(input_path, output_path)
+        elif os.path.isdir(input_path):
+            print(f"Обрабатываем директорию: {input_path}")
+            compressor.process_directory(input_path)
+    else:
+        print("Указанный путь не существует")
+
+if __name__ == "__main__":
+    user_input = input("Введите путь к файлу или директории: ")
+    main(user_input)
+
+
